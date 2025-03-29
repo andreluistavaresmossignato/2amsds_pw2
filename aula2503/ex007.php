6ecac57a61d14e4ab2e879f60c9e0274020a415e
André Luís Tavares Mossignato <andreluis.mossignato@gmail.com>
2025-03-29 08:38:38 -0300

    exercício 7 de array

--- aula2503/ex007.php
+++ aula2503/ex007.php
@@ -1,3 +1,11 @@
 <?php 
 
-    
+    $a = [10, 3, 30, 45, 6, 78, 4, 33, 89, 0];
+    $posic;
+
+    for ($i=0; $i < sizeof($a); $i++) { 
+        $posic = $i + 1;
+        if($a[$i] <= 10) {
+            echo " $a[$i] na posição $posic" . "\n";
+        }
+    }     
