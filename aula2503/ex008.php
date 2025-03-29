e4acd698a5fe144126a1e9c98ab128a41c62cd05
André Luís Tavares Mossignato <andreluis.mossignato@gmail.com>
2025-03-29 09:37:59 -0300

    Exercício 8 de vetores

---
+++ aula2503/ex008.php
@@ -0,0 +1,11 @@
+<?php 
+
+    $n = [2];
+    $posic;
+
+  for ($i=0; $i < 10; $i++) { 
+        $sub = $n[$i] * 2;
+        $posic = $i + 1;
+        $n[$posic] = $sub;
+        echo "$n[$i]" . "\n";
+    } 
